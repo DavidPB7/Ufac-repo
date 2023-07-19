e08b3a9dc2831f072f21342bdbc5d5cdadfad685
DavidPB7 <davidpereira1701gmail.com>
2023-07-19 07:23:26 -0500

    Tesii01
    novas telas e funcionalidades de tesi01

--- Tesi01/telasBanco/pages/perfil.py
+++ Tesi01/telasBanco/pages/perfil.py
@@ -12,7 +12,7 @@ class Tela:
 
 
         # Label Frame 01
-        self.lbl_frm_one = tk.LabelFrame(self.janela, text='Minha Conta', font=('Tahoma', 12), height=100, width=100)
+        self.lbl_frm_one = tk.LabelFrame(self.janela, text='Minha Conta: ', font=('Tahoma', 12), height=100, width=100)
         self.lbl_frm_one.pack(anchor=tk.CENTER, pady=10)
 
         self.lbl_one = tk.Label(self.lbl_frm_one, text='Foto', borderwidth=4, relief='groove', pady=30, padx=30)
@@ -32,7 +32,7 @@ class Tela:
 
         # Label Frame 02
 
-        self.lbl_frm_three = tk.LabelFrame(self.janela, text='Funcionalidades', font=('Tahoma', 12), height=100, width=100, border=0)
+        self.lbl_frm_three = tk.LabelFrame(self.janela, text='Funcionalidades: ', font=('Tahoma', 12), height=100, width=100, border=0)
         self.lbl_frm_three.pack(anchor=tk.CENTER, pady=10)
 
         self.lbl_one = tk.Button(self.lbl_frm_three, text='Transferir', borderwidth=2, relief='groove', pady=15, padx=15, command=self.telaTransfere)
@@ -41,25 +41,17 @@ class Tela:
         self.lbl_two = tk.Button(self.lbl_frm_three, text='Sacar', borderwidth=2, relief='groove', pady=15, padx=15, command=self.telaSaca)
         self.lbl_two.grid(row=0, column=1)
 
-        self.lbl_three = tk.Button(self.lbl_frm_three, text='Depositar', borderwidth=2, relief='groove', pady=15, padx=15)
+        self.lbl_three = tk.Button(self.lbl_frm_three, text='Depositar', borderwidth=2, relief='groove', pady=15, padx=15, command=self.telaDeposito)
         self.lbl_three.grid(row=0, column=2)
 
-        self.four_pass = tk.Button(self.lbl_frm_three, text='Investir', borderwidth=2, relief='groove', pady=15, padx=15)
+        self.four_pass = tk.Button(self.lbl_frm_three, text='Relatórios', borderwidth=2, relief='groove', pady=15, padx=15)
         self.four_pass.grid(row=0, column=3)
         #
         # self.lbl_four = tk.Button(self.lbl_frm_three, text='Detalhes da conta')
         # self.lbl_four.grid(row=2, column=0, columnspan=2)
 
-        # Label Frame 03
-        self.lbl_frm_two = tk.LabelFrame(self.janela, font=('Tahoma', 12), height=100, width=100)
-        self.lbl_frm_two.pack(anchor=tk.CENTER)
 
-        self.btn_one = tk.Button(self.lbl_frm_two, text='Boleto', borderwidth=1, relief='ridge', pady=30, padx=30)
-        self.btn_one.grid(row=0, column=0)
-
-        self.btn_two = tk.Button(self.lbl_frm_two, text='Vantagens', borderwidth=1, relief='ridge', pady=30, padx=30)
-        self.btn_two.grid(row=0, column=1)
-
+    # Tela detalhes da conta
     def detalhes(self):
         self.janela_detalhes = self.janela
 
@@ -76,9 +68,13 @@ class Tela:
         btn_voltar = tk.Button(self.janela_detalhes, text="Voltar", borderwidth=1, relief='ridge', command=voltar)
         btn_voltar.pack(anchor="nw", pady=5, padx=5)
 
-
+    #  Tela para sacar
     def telaSaca(self):
         self.janela_principal = self.janela  # Salvar a referência à janela principal
+
+        #Variaveis do Entry para validação
+        self.validate_valor = tk.StringVar()
+        self.validate_valor.set(' ')
 
         self.janela.withdraw()
         self.janela_saca = tk.Toplevel()
@@ -94,7 +90,7 @@ class Tela:
         btn_voltar = tk.Button(self.janela_saca, text="Voltar", borderwidth=1, relief='ridge', command=voltar)
         btn_voltar.pack(anchor="nw", pady=5, padx=5)
 
-        self.frm_label1 = tk.LabelFrame(self.janela_saca, text='Area de Saque')
+        self.frm_label1 = tk.LabelFrame(self.janela_saca, text='Area de Saque', font=('Tahoma', 12))
         self.frm_label1.pack()
 
         self.lbl01 = tk.Label(self.frm_label1, text="Qual é o valor do Saque?")
@@ -103,15 +99,29 @@ class Tela:
         self.lbl02 = tk.Label(self.frm_label1, text="Saldo disponível em conta para sacar: R$40,44")
         self.lbl02.pack()
 
-        self.lbl03 = tk.Entry(self.frm_label1)
+        self.lbl03 = tk.Entry(self.frm_label1, textvariable=self.validate_valor)
         self.lbl03.insert(tk.END, "R$0,00")
         self.lbl03.pack(pady=15, padx=10)
 
-        self.lbl04 = tk.Button(self.frm_label1, text="Sacar", width=7, height=1, font=4, bg="#50fa7d", activebackground = '#0afa49')
+        self.rbt1 = tk.Radiobutton(self.frm_label1, text='Conta Poupança', value='pp')
+        self.rbt1.pack()
+        self.rbt2 = tk.Radiobutton(self.frm_label1, text='Conta Corrente', value='pc')
+        self.rbt2.pack()
+
+        # def confirmar():
+        #     valor = self.validate_valor
+
+        #     if (valor == '') or (valor == 0):
+        #         messagebox.showinfo('Aviso', 'Valor está incorreto! Tente novamente!')
+        #     else:
+        #         self.janela.withdraw()
+        #         self.home_page()
+
+        self.lbl04 = tk.Button(self.frm_label1, text="Sacar", width=7, height=1, font=4, bg="#50fa7d", activebackground = '#0afa49', command=confirmar)
         self.lbl04.pack(pady=5)
 
         
-
+    # Tela para transfência
     def telaTransfere(self):
         self.janela_principal = self.janela  # Salvar a referência à janela principal
 
@@ -129,20 +139,64 @@ class Tela:
         btn_voltar = tk.Button(self.janela_transfere, text="Voltar", borderwidth=1, relief='ridge', command=voltar)
         btn_voltar.pack(anchor="nw", pady=5, padx=5)
 
-        self.frm_label2 = tk.LabelFrame(self.janela_transfere, text='Area pix')
+        self.frm_label2 = tk.LabelFrame(self.janela_transfere, text='Area pix', font=('Tahoma', 12))
         self.frm_label2.pack()
 
         self.lbl01 = tk.Label(self.frm_label2, text="Qual é o valor da transferência?")
         self.lbl01.pack()
 
-        self.lbl02 = tk.Label(self.frm_label2, text="Saldo didponível em conta: R$40,44")
+        self.lbl02 = tk.Label(self.frm_label2, text="Saldo disponível em conta: R$40,44")
         self.lbl02.pack()
 
         self.lbl03 = tk.Entry(self.frm_label2)
         self.lbl03.insert(tk.END, "R$0,00")
         self.lbl03.pack(pady=15, padx=10)
 
+        self.rbt1 = tk.Radiobutton(self.frm_label2, text='Conta Poupança', value='pp')
+        self.rbt1.pack()
+        self.rbt2 = tk.Radiobutton(self.frm_label2, text='Conta Corrente', value='pc')
+        self.rbt2.pack()
+
         self.lbl04 = tk.Button(self.frm_label2, text="Confirmar", width=7, height=1, font=4, bg="#50fa7d", activebackground = '#0afa49')
+        self.lbl04.pack(pady=5)
+
+    # Tela para Deposito
+    def telaDeposito(self):
+        self.janela_principal = self.janela  # Salvar a referência à janela principal
+
+        self.janela.withdraw()
+        self.janela_deposito = tk.Toplevel()
+
+        # Configurações da nova janela
+        self.janela_deposito.title("Transferência")
+        self.janela_deposito.geometry("400x400")
+
+        def voltar():
+            self.janela_deposito.destroy()
+            self.janela_principal.deiconify()  # Restaurar a janela principal
+
+        btn_voltar = tk.Button(self.janela_deposito, text="Voltar", borderwidth=1, relief='ridge', command=voltar)
+        btn_voltar.pack(anchor="nw", pady=5, padx=5)
+
+        self.frm_label3 = tk.LabelFrame(self.janela_deposito, text='Area Deposito', font=('Tahoma', 12))
+        self.frm_label3.pack()
+
+        self.lbl01 = tk.Label(self.frm_label3, text="Qual é o valor da Deposito?")
+        self.lbl01.pack()
+
+        self.lbl02 = tk.Label(self.frm_label3, text="Saldo disponível em conta: R$40,44")
+        self.lbl02.pack()
+
+        self.lbl03 = tk.Entry(self.frm_label3)
+        self.lbl03.insert(tk.END, "R$0,00")
+        self.lbl03.pack(pady=15, padx=10)
+
+        self.rbt1 = tk.Radiobutton(self.frm_label3, text='Conta Poupança', value='pp')
+        self.rbt1.pack()
+        self.rbt2 = tk.Radiobutton(self.frm_label3, text='Conta Corrente', value='pc')
+        self.rbt2.pack()
+
+        self.lbl04 = tk.Button(self.frm_label3, text="Confirmar", width=7, height=1, font=4, bg="#50fa7d", activebackground = '#0afa49')
         self.lbl04.pack(pady=5)
 
             
