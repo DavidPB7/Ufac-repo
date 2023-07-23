937dc8feeca15a81d19bf69f82fe6f44a8771d58
David Pereira <120596420+DavidPB7@users.noreply.github.com>
2023-07-23 15:10:31 -0500

    Update prova01.c
    Não ficou certo, vou estudar mais e corrigir depois!

--- SD/prova01.c
+++ SD/prova01.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 /*Definir o grafo da aplicação antes de executar*/
@@ -9,16 +10,17 @@ int matrizVizinhanca[6][6] = {{0,1,1,0,0,0},
                               {0,1,0,0,0,0},
                               {0,1,0,0,0,0},
                               {0,0,1,0,0,0}};
+
 /*retorna o número de vizinhos da tarefa myRank*/
 int contaNumeroDeVizinhos(int myRank)
 {
    int i;
    int contador = 0;
-   
+
    for (i = 0; i < numeroDeTarefas; i++)
       if (matrizVizinhanca[myRank][i] == 1)
          contador++;
-         
+
    return contador;
 }
 /*programa principal*/
@@ -30,8 +32,8 @@ int main(int argc, char** argv)
    int source;
    int tag = 50;
    int pai;
-   int contador;
-   char message[100] = "Oi!";
+   int cont = 0;
+
 
    MPI_Status status;
    //inicialização do MPI
@@ -39,48 +41,46 @@ int main(int argc, char** argv)
    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
    numeroDeVizinhos = contaNumeroDeVizinhos(myRank);
 
-
+   printf("-----Q1-----\n");
    if (myRank == 0)
-	{
+        {
            /*enviando para todos os vizinhos de myRank*/
-           for (i = 0; i < numeroDeTarefas; i++)	
+           for (i = 0; i < numeroDeTarefas; i++)
                if (matrizVizinhanca[myRank][i] == 1)
                {
-            	   printf("Enviando a informação para %d\n", i);
-		   MPI_Send(message, 100, MPI_CHAR, i, tag, MPI_COMM_WORLD);
-		   contador = contador + 1
+                   printf("Enviando a informação para %d\n", i);
+                   cont = cont + 1;
+                   MPI_Send(&cont, 1, MPI_INT, i, tag,
+                     MPI_COMM_WORLD);
                }
-            
-           /*recebendo de todos os vizinhos de myRank*/      
+
+           /*recebendo de todos os vizinhos de myRank*/
+
            for (i = 0; i < numeroDeTarefas; i++)
                if (matrizVizinhanca[myRank][i] == 1)
                {
                    printf("Recebendo a informação de %d\n", i);
-                   MPI_Recv(message, 100, MPI_CHAR, i, tag,
+                   MPI_Recv(&cont, 1, MPI_INT   , i, tag,
                      MPI_COMM_WORLD, &status);
+                     printf("A distancia até o nó de origem até o nó %d foi de: %d\n", i, cont);
                }
    }
    else {
-
       /*recebendo msg de uma tarefa vizinha qualquer*/
-      MPI_Recv(message, 100, MPI_CHAR, MPI_ANY_SOURCE, tag,MPI_COMM_WORLD, &status);
+      MPI_Recv(&cont, 1, MPI_INT, MPI_ANY_SOURCE, tag,MPI_COMM_WORLD, &status);
       pai = status.MPI_SOURCE;
 
       /*enviando para todos os vizinhos de myRank menos seu pai*/
-      for (i = 0; i < numeroDeTarefas; i++)	
-         if ( (matrizVizinhanca[myRank][i] == 1) && (i != pai) ){
-            MPI_Send(message, 100, MPI_CHAR, i, tag, MPI_COMM_WORLD);
-         }
-      /*recebendo de todos os vizinhos de myRank menos 1*/      
-      for(i = 0; i < (numeroDeVizinhos - 1); i++) {	
-         MPI_Recv(message, 100, MPI_CHAR, MPI_ANY_SOURCE, tag,MPI_COMM_WORLD, &status);
+      for (i = 0; i < numeroDeTarefas; i++)
+      if ( (matrizVizinhanca[myRank][i] == 1) && (i != pai) ){
+         cont = cont + 1;
+         MPI_Send(&cont, 1, MPI_INT, i, tag,MPI_COMM_WORLD);
+      }
+      /*recebendo de todos os vizinhos de myRank menos 1*/
+      for(i = 0; i < (numeroDeVizinhos - 1); i++) {
+         MPI_Recv(&cont, 1, MPI_INT, MPI_ANY_SOURCE, tag,MPI_COMM_WORLD, &status);
        }
-       MPI_Send(message, 100, MPI_CHAR, pai, tag,MPI_COMM_WORLD);
-       if(numeroDeVizinhos == 1) {
-	printf("A distancia do nó %d até o fim é de: %d", MyRank,contador);
-
-
-}
+       MPI_Send(&cont, 1, MPI_INT, pai, tag,MPI_COMM_WORLD);
    }
    //Finalização do MPI
    MPI_Finalize();
